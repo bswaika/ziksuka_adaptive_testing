fd26ffb52604d7073bd5939d52830e15c8821aaf
Baladitya Swaika <swaika@usc.edu>
2022-03-24 11:32:25 -0700

    Final codebase.

--- adaptive_testing_API_pre_prod/adaptive/settings.py
+++ adaptive_testing_API_pre_prod/adaptive/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9(o%^e476-m=*o(oi(+vq(+gktd=v3a9ea53aoyn!fqou=&s(n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
